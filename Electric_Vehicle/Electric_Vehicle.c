6aee9b6e9920880d711c9c2040b5067785a542ce
Jason Li <jcl01010011@gmail.com>
2016-04-14 15:26:32 -0400

    Rewrote electric vehicle

--- Electric_Vehicle/Electric_Vehicle.c
+++ Electric_Vehicle/Electric_Vehicle.c
@@ -1,40 +1,55 @@
 #define F_CPU 16000000L
 
-#include <stdint.h>
-#include <stdio.h>
-#include <stm8/UART.h>
 #include <stm8/stm8s103.h>
 #include <stm8/timing.h>
 
 volatile unsigned int count = 0;
+unsigned int count_max = 0;
 
-const unsigned int count_max = 0;
-
-void encoder_pulse() __interrupt(3) { //EXTI0 (Port A interrupts)
+void got_pulse() __interrupt(6) {
 	++count;
-	PB_ODR ^= 0x20; //toggle PB5
+	PB_ODR ^= (1<<5);
 }
 
 void main() {
+	unsigned char i = 0;
+	
 	CLK_CKDIVR = 0x00; // Set the frequency to 16 MHz
 	CLK_PCKENR1 = 0xFF; // Enable peripherals
 	
-	PA_DDR = 0x0C; //output on PA2, PA3
-	PA_CR1 = 0x0C; //push/pull on PA2, PA3
-	PA_CR2 = 0x02; //enable interrupt on PA1
+	PA_DDR = (1<<1)|(1<<2); //output on PA1,2
+	PA_CR1 = (1<<1)|(1<<2)|(1<<3); //push-pull on PA1,2, pullup on PA3
+	//TODO: find out what is necessary to get the h-bridge to stop
 	
-	PB_DDR = 0x20; //output on PB5
-	PB_CR1 = 0x20;
+	PB_DDR = (1<<5); //output on PB5
+	PB_CR1 = (1<<5); //push-pull on PB5
 	
-	PD_CR1 = 0x10; //pull-up on PD4
+	//no outputs on port C, the outputs will be done manually
+	PC_ODR = (1<<2)|(1<<3)|(1<<4)|(1<<5); //preload high on PC2,3,4,5
+	PC_CR1 = (1<<6)|(1<<7); //pullup on PC6,7
 	
-	while(!(PD_IDR&0x10)); //wait until start button pressed
+	//no outputs on port D
+	PD_CR1 = (1<<2)|(1<<3); //pullup on PC2,3
+	PD_CR2 = (1<<4); //enable external interrupt on PD4
 	
-	PA_ODR = 0x08; //start motor
-	__asm rim __endasm; //enable interrupts
+	PC_DDR = (1<<5);
+	for (;i<4;++i) {
+		pause(1);
+		count_max |= (((PC_ODR&(1<<7))>>5) |
+			((PC_ODR&(1<<6))>>3) |
+			((PD_ODR&(1<<2))>>1) |
+			((PD_ODR&(1<<3))>>3))<<(i<<2);
+		PC_DDR <<= 1;
+	}
+	PC_DDR = 0; //read pins
 	
-	while(count<count_max); //wait until count is big enough
+	__asm rim __endasm; //enable interrupt
 	
-	__asm sim __endasm; //disable interrupts
-	PA_ODR = 0x00; //stop motor
+	while (PA_IDR & (1<<3)); //wait for start
+	PA_ODR |= (1<<1); //TODO: whatever is needed to start h-bridge
+	while (count<count_max);
+	PA_ODR &= ~(1<<1); //TODO: whatever is needed to stop h-bridge
+	
+	PD_CR2 &= ~(1<<4); //disable external interrupts on PD4
+	__asm halt __endasm; //halt
 }
