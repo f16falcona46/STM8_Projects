6e1efbd2f8b3b38bdad98a6aef834d4fd9e47f4f
Jason Li <jcl01010011@gmail.com>
2016-05-17 09:58:22 -0400

    Hopefully committed Electric Vehicle v2

--- Electric_Vehicle/Electric_Vehicle.c
+++ Electric_Vehicle/Electric_Vehicle.c
@@ -3,11 +3,51 @@
 #include <stm8/stm8s.h>
 #include <stm8/timing.h>
 
+/*
+input pin
+PB5
+
+LED pin
+PD7
+
+motor pins
+PA1
+PA2
+
+start button pin
+PF4
+
+configuration DIP switch
+HIGH
+PD3 15
+PD2 14
+PD0 13
+
+PC7 12
+PC6 11
+PC5 10
+PC4 9
+PC3 8
+PC2 7
+PC1 6
+
+PE5 5
+
+PB0 4
+PB1 3
+PB2 2
+PB3 1
+PB4 0
+LOW
+*/
+
+
+
 volatile unsigned int count = 0;
 
 void got_pulse() __interrupt(EXTI1_VECT) {
 	++count;
-	PB_ODR ^= (1<<5);
+	PD_ODR ^= (1<<7);
 }
 
 void main() {
@@ -16,11 +56,26 @@ void main() {
 	CLK_CKDIVR = 0x00; // Set the frequency to 16 MHz
 	CLK_PCKENR1 = 0xFF; // Enable peripherals
 	
-	//init port A
-	//init port B
-	//init port C
-	//init port D
-	//init port E
+	PA_DDR = 0x06;
+	PD_CR1 = 0x06;
+	
+	PB_CR1 = 0x3F;
+	PB_CR2 = 0x20;
+	
+	PC_CR1 = 0xFE;
+	
+	PD_DDR = 0x80;
+	PD_CR1 = 0x8F;
+	
+	PE_CR1 = 0x20;
+	
+	PF_CR1 = 0x10;
+	
+	pause(1);
+	
+	while (PF_IDR & (1<<4)); //wait for start
+	
+	pause(1);
 	
 	count_max |= (PD_IDR & ((1<<3)|(1<<2)))<<(12);
 	count_max |= (PD_IDR & (1<<0))<<(13);
@@ -32,15 +87,12 @@ void main() {
 	count_max |= (PB_IDR & (1<<3))>>2;
 	count_max |= (PB_IDR & (1<<4))>>4;
 	
-	while (PA_IDR & (1<<3)); //wait for start
-	
-	
 	__asm rim __endasm; //enable interrupt
 	while (count<count_max);
 	__asm halt __endasm; //halt
 	
 	PA_ODR &= ~(1<<1); //TODO: whatever is needed to stop h-bridge
-	PD_CR2 &= ~(1<<4); //disable external interrupts on PD4
+	PB_CR2 &= ~(1<<5); //disable external interrupts on PD4
 	
 	while (1);
 }
