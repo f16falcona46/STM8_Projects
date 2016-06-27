6f6861c1f2d1806b94cb1b8c8817cb2e4bd9a88a
f16falcona46 <li07.jason@gmail.com>
2016-06-26 21:39:00 -0400

    Added SPI includes and commands for Macronix flash memory

---
+++ STM8_Includes/stm8/SPI.h
@@ -0,0 +1,30 @@
+#ifndef SPI_FUNCTIONS_INCLUDES
+#define SPI_FUNCTIONS_INCLUDES
+
+uint8_t SPI_transfer(uint8_t output) {
+	while (!(SPI_SR&(1<<1)));
+	SPI_DR = output;
+	while (!(SPI_SR&(1<<0)));
+	output = SPI_DR;
+	return output;
+}
+
+void SPI_transfer_buf(uint8_t* source, uint8_t* dest, uint16_t len) {
+	uint16_t index = 0;
+	for (;index < len; ++index) {
+		dest[index] = SPI_transfer(source[index]);
+	}
+}
+
+void SPI_cmd(uint8_t* cmd, uint16_t cmd_len, uint8_t* dest, uint16_t dest_len) {
+	uint16_t index = 0;
+	PC_ODR &= ~(1<<4); //assert CS# low
+	for (;index < cmd_len; ++index) {
+		SPI_transfer(cmd[index]);
+	}
+	for (index = 0; index < dest_len; ++index) {
+		dest[index] = SPI_transfer(0);
+	}
+	PC_ODR |= (1<<4); //release CS# (make it high)
+}
+#endif
